1ab5abaa4d78718b607275704dd8d6a8dbd61e8c
Tommy Ru <TommyRu@quickenloans.com>
2016-09-24 21:25:15 -0400

    controller will return render function instead of array

--- src/Controllers/Calc/Get.php
+++ src/Controllers/Calc/Get.php
@@ -9,16 +9,21 @@ namespace QL\CJarvis\MVC\Controllers\Calc;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use QL\CJarvis\MVC\libs\ControllerInterface;
+use QL\CJarvis\MVC\libs\Template;
 
 class Get implements ControllerInterface
 {
     /**
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
-     * @return array
+     * @param Template $template
+     * @return string|null
      */
-    public function __invoke(ServerRequestInterface $request, ResponseInterface $response)
-    {
-        return [];
+    public function __invoke(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        Template $template
+    ) {
+        return $template->render([]);
     }
 }
